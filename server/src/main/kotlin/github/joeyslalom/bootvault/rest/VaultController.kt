d01523f0d0819974d1e64bb3aafc3e548bdae8ff
joeyt <joeyt@slalom.com>
2018-04-07 21:04:21 -0700

    Encrypt/decrypt

--- server/src/main/kotlin/github/joeyslalom/bootvault/rest/VaultController.kt
+++ server/src/main/kotlin/github/joeyslalom/bootvault/rest/VaultController.kt
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.vault.core.VaultOperations
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 
 @Controller
 class VaultController {
@@ -15,8 +17,15 @@ class VaultController {
 class StsController : StsApi
 
 @Controller
-class TransitController(private val vaultOpts: VaultOperations) : TransitApi {
+class TransitController(private val vaultOps: VaultOperations) : TransitApi {
+    private val transitOps = vaultOps.opsForTransit()
     private val log = LoggerFactory.getLogger(TransitController::class.java)
 
-    override fun getListKeys(): ResponseEntity<List<String>> = ResponseEntity.ok(vaultOpts.opsForTransit().keys)
+    override fun getListKeys(): ResponseEntity<List<String>> = ResponseEntity.ok(transitOps.keys)
+
+    override fun putEncrypt(@PathVariable key: String, @RequestBody plainText: String): ResponseEntity<String> =
+            ResponseEntity.ok(transitOps.encrypt(key, plainText))
+
+    override fun putDecrypt(@PathVariable key: String, @RequestBody cipherText: String): ResponseEntity<String> =
+            ResponseEntity.ok(transitOps.decrypt(key, cipherText))
 }
