84f8a0f540ff680aabdce664db30fad7b9d5e6a1
joeyt <joeyt@slalom.com>
2018-04-07 20:34:48 -0700

    List keys endpoint working

--- server/src/main/kotlin/github/joeyslalom/bootvault/rest/VaultController.kt
+++ server/src/main/kotlin/github/joeyslalom/bootvault/rest/VaultController.kt
@@ -2,7 +2,10 @@ package github.joeyslalom.bootvault.rest
 
 import github.joeyslalom.bootvault.rest.api.StsApi
 import github.joeyslalom.bootvault.rest.api.TransitApi
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.vault.core.VaultOperations
 
 @Controller
 class VaultController {
@@ -12,4 +15,8 @@ class VaultController {
 class StsController : StsApi
 
 @Controller
-class TransitController : TransitApi
+class TransitController(private val vaultOpts: VaultOperations) : TransitApi {
+    private val log = LoggerFactory.getLogger(TransitController::class.java)
+
+    override fun getListKeys(): ResponseEntity<List<String>> = ResponseEntity.ok(vaultOpts.opsForTransit().keys)
+}
