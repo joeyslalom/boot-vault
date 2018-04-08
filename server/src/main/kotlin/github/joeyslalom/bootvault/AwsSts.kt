5cbd18accb24c319f619b67afc0d6bcd0e37ab7f
joeyt <joeyt@slalom.com>
2018-04-07 22:23:43 -0700

    Get STS creds to publish

---
+++ server/src/main/kotlin/github/joeyslalom/bootvault/AwsSts.kt
@@ -0,0 +1,66 @@
+package github.joeyslalom.bootvault
+
+import com.amazonaws.auth.AWSSessionCredentials
+import com.amazonaws.auth.AWSSessionCredentialsProvider
+import com.amazonaws.services.sns.AmazonSNSAsync
+import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.vault.annotation.VaultPropertySource
+
+
+private const val STS_PREFIX_SNS = "sts.sns."
+
+
+@Configuration
+@VaultPropertySource(
+        value = ["aws/sts/use1-ucp-dev-snsaccess"],
+        renewal = VaultPropertySource.Renewal.ROTATE,
+        propertyNamePrefix = STS_PREFIX_SNS
+)
+class SnsVaultProps(private val env: Environment) : CredentialsProvider {
+    override fun credentials() = Credentials.of(STS_PREFIX_SNS, env)
+}
+
+interface CredentialsProvider : AWSSessionCredentialsProvider {
+    fun credentials(): Credentials
+
+    override fun getCredentials(): AWSSessionCredentials = credentials()
+
+    override fun refresh() {
+    }
+}
+
+class Credentials(
+        val accessKey: String,
+        val secretKey: String,
+        val securityToken: String) : AWSSessionCredentials {
+
+    override fun getSessionToken(): String = securityToken
+
+    override fun getAWSAccessKeyId(): String = accessKey
+
+    override fun getAWSSecretKey(): String = secretKey
+
+    companion object {
+        fun of(prefix: String, env: Environment) = Credentials(
+                env.getProperty("${prefix}access_key", ""),
+                env.getProperty("${prefix}secret_key", ""),
+                env.getProperty("${prefix}security_token", "")
+        )
+    }
+}
+
+private const val AWS_REGION = "us-east-1"
+
+@Configuration
+class Config {
+    @Bean
+    fun amazonSns(provider: SnsVaultProps): AmazonSNSAsync {
+        return AmazonSNSAsyncClientBuilder.standard()
+                .withRegion(AWS_REGION)
+                .withCredentials(provider)
+                .build()
+    }
+}
--- server/src/main/kotlin/github/joeyslalom/bootvault/rest/VaultController.kt
+++ server/src/main/kotlin/github/joeyslalom/bootvault/rest/VaultController.kt
@@ -1,6 +1,7 @@
 package github.joeyslalom.bootvault.rest
 
-import github.joeyslalom.bootvault.rest.api.StsApi
+import github.joeyslalom.bootvault.SnsVaultProps
+import github.joeyslalom.bootvault.rest.api.AwsApi
 import github.joeyslalom.bootvault.rest.api.TransitApi
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
@@ -9,12 +10,21 @@ import org.springframework.vault.core.VaultOperations
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 
-@Controller
-class VaultController {
-}
 
 @Controller
-class StsController : StsApi
+class AwsController(private val snsVaultProps: SnsVaultProps) : AwsApi {
+    private val log = LoggerFactory.getLogger(AwsController::class.java)
+
+    override fun getSnsCreds(): ResponseEntity<Void> {
+        val credentials = snsVaultProps.credentials()
+        log.info("""copy and paste SNS creds below
+export AWS_ACCESS_KEY_ID=${credentials.accessKey}
+export AWS_SECRET_ACCESS_KEY=${credentials.secretKey}
+export AWS_SESSION_TOKEN=${credentials.securityToken}
+""")
+        return ResponseEntity.noContent().build()
+    }
+}
 
 @Controller
 class TransitController(private val vaultOps: VaultOperations) : TransitApi {
