a003560c15ab01b9f20b1d308d04bdabc161cb71
joeyt <joeyt@slalom.com>
2018-06-05 11:41:49 -0700

    Add shell

---
+++ shell/src/main/kotlin/github/joeyslalom/ShellApp.kt
@@ -0,0 +1,13 @@
+package github.joeyslalom
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ShellApp
+
+fun main(args: Array<String>) {
+    SpringApplication.run(ShellApp::class.java, *args)
+}
+
+
